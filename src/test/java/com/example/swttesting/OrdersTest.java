c41b56bf8b85fdb248a9310f4ceb667cd5a9f313
marwannour1 <marwannour20022016@gmail.com>
2024-04-18 19:05:59 +0200

    zbt el sho8l

---
+++ src/test/java/com/example/swttesting/OrdersTest.java
@@ -0,0 +1,54 @@
+package com.example.swttesting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdersTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getOrderID() {
+
+    }
+
+    @Test
+    void setOrderID() {
+    }
+
+    @Test
+    void getOrderDate() {
+    }
+
+    @Test
+    void setOrderDate() {
+    }
+
+    @Test
+    void getOrderStatus() {
+    }
+
+    @Test
+    void setOrderStatus() {
+    }
+
+    @Test
+    void getOrderTotal() {
+    }
+
+    @Test
+    void setOrderTotal() {
+    }
+
+    @Test
+    void getShoppingCart() {
+    }
+
+    @Test
+    void setShoppingCart() {
+    }
+}
