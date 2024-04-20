09a73fbe824fdc8c31964d2ceeca8c4b59a2b2d3
marwannour1 <marwannour20022016@gmail.com>
2024-04-20 12:26:32 +0200

    begin unit_testing

---
+++ src/test/java/com/example/swttesting/UserTest.java
@@ -0,0 +1,62 @@
+package com.example.swttesting;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void getShoppingCart() {
+    }
+
+    @Test
+    void getOrders() {
+    }
+
+    @Test
+    void setShoppingCart() {
+    }
+
+    @Test
+    void addOrder() {
+    }
+
+    @Test
+    void addProductToCart() {
+    }
+}
